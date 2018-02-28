12a1ababd343768d3c523ebaf818020bd1980c5b
moon <hoop_ster@naver.com>
2018-02-28 16:51:01 +0900

    -18.02.28 analysis 1st fix

---
+++ src/com/cl/persistance/mapper/AnalysisMapper.java
@@ -0,0 +1,20 @@
+package com.cl.persistance.mapper;
+
+import java.util.List;
+
+import com.cl.config.Mapper;
+import com.cl.dto.AnalysisDTO;
+
+@Mapper("AnalysisMapper")
+public interface AnalysisMapper {
+
+	List<AnalysisDTO> getSexAnalysisData() throws Exception;
+
+	List<AnalysisDTO> getAgeAnalysisData() throws Exception;
+
+	List<AnalysisDTO> getReRegAnalysis() throws Exception;
+
+	List<AnalysisDTO> getRegYearAnalysis() throws Exception;
+
+	List<AnalysisDTO> getStateAnalysis() throws Exception;
+}
